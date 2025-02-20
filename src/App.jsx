15b3674793900591ece5635ff4d588eb9be39ed3
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-20 20:09:01 +0200

    Create layout and add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import './App.css'
+import './App.css';
+import { BUTTONS } from './constants';
+
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
 
 function App() {
     return (
         <>
+            <div className='main-wrapper'>
+                <div>
+                    <div className='result-screen'>
+                    </div>
+                </div>
+                <div className='buttons-wrapper'>
+                    <ButtonBox>
+                        {BUTTONS.flat().map((btn, i) => {
+                            return <Button
+                                key={i}
+                                className={btn === '=' ? 'long' : 'button'}
+                                value={btn}
+                            />
+                        })}
+                    </ButtonBox>
+                </div>
+            </div>
         </>
     )
 }
