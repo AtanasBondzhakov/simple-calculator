74d101cf33ab38256188799ff27cf03e6a9e5968
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-20 20:17:06 +0200

    Add updater state function
    Add button click handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
+import { useState } from 'react';
+
 import './App.css';
 import { BUTTONS } from './constants';
+import { OPERATORS } from './constants';
 
 import ButtonBox from './components/ButtonBox';
 import Button from './components/Button';
 
 function App() {
+    const [calc, setCalc] = useState({
+        num: 0,
+        sign: '',
+        result: ''
+    });
+
+    const isOperator = (value) => Object.keys(OPERATORS).includes(value);
+
+    const updateCalc = (updates) => setCalc(prevCalc => ({ ...prevCalc, ...updates }));
+
+    const buttonsMap = {
+        'C': clearHandler,
+        '=': equalHandler,
+        '%': percentageHandler,
+        '+-': plusMinusHandler
+    };
+
+    const buttonClickHandler = (value) => {
+        if (buttonsMap[value]) {
+            buttonsMap[value]();
+        } else if (isOperator(value)) {
+            signHandler(value);
+        } else {
+            numberDotHandler(value);
+        }
+    };
+
     return (
         <>
             <div className='main-wrapper'>
                 <div>
                     <div className='result-screen'>
+                        {calc.num || calc.result || 0}
                     </div>
                 </div>
                 <div className='buttons-wrapper'>
@@ -18,6 +49,7 @@ function App() {
                             return <Button
                                 key={i}
                                 className={btn === '=' ? 'long' : 'button'}
+                                onClick={() => buttonClickHandler(btn)}
                                 value={btn}
                             />
                         })}
